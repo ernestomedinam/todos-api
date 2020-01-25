9ac2551af07a6986a340b55f7dd4b3344555ee80
Ernesto Medina <ernesto.m.medina.m@gmail.com>
2020-01-25 02:52:45 +0000

    cleared commented old code.

--- src/models.py
+++ src/models.py
@@ -27,17 +27,3 @@ class Todo(db.Model):
             "label": self.label,
             "done": self.done
         } 
-
-# class Person(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-
-#     def __repr__(self):
-#         return '<Person %r>' % self.username
-
-#     def serialize(self):
-#         return {
-#             "username": self.username,
-#             "email": self.email
-#         }
